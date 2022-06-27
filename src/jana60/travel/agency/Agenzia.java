6a32233d6a26725d8514d62283c07e7c4bf7714c
LauraElenaAnghelache <laura.elena.anghelache@outlook.it>
2022-06-27 11:40:39 +0200

    first commit

---
+++ src/jana60/travel/agency/Agenzia.java
@@ -0,0 +1,5 @@
+package jana60.travel.agency;
+
+public class Agenzia {
+
+}
---
+++ src/jana60/travel/agency/Vacanza.java
@@ -0,0 +1,10 @@
+package jana60.travel.agency;
+
+public class Vacanza {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
